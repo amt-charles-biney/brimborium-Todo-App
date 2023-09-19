2e348c01e33096e1f83cc872122bfde2f88a0017
Charles Biney <charles.biney@amalitech.org>
2023-09-19 11:23:42 +0000

    reset: codebase reset

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { TodoModule } from './todo/todo.module';
-import { UsersModule } from './users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './auth/local.strategy';
-import { SessionSerializer } from './auth/session.serializer';
-import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [
-    UsersModule,
-    TodoModule,
-    AuthModule,
-    PassportModule.register({ session: true }),
-  ],
-  providers: [AuthService, LocalStrategy, SessionSerializer],
+  imports: [],
+  providers: [],
 })
 export class AppModule {}
