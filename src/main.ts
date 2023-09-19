c6562ef97bde4ba5a33f15ef0a3ece9ff4c14f2c
Charles Biney <charles.biney@amalitech.org>
2023-09-19 14:12:27 +0000

    feat: validate createUserDto

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   });
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(2, 255)
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @Length(8, 255)
   password: string;
 }
