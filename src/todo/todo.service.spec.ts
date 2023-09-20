7b5dc765bbec3dea031e5eec771ab4338c9f8a2f
Charles Biney <charles.biney@amalitech.org>
2023-09-20 16:05:31 +0000

    feat: get todo service with test

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,9 +1,7 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaService } from '../prisma.service';
-import { UserService } from '../user/user.service';
-import { CreateTaskDTO } from './dtos';
 import { TodoService } from './todo.service';
+import { PrismaService } from '../prisma.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('TodoService', () => {
   let todoService: TodoService;
@@ -11,67 +9,57 @@ describe('TodoService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService, PrismaService, UserService],
+      providers: [TodoService, PrismaService],
     }).compile();
 
     todoService = module.get<TodoService>(TodoService);
     prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should get a task successfully', async () => {
-    const taskId = '1';
-    const taskData = {
-      id: taskId,
-      topic: 'Task 1',
-      description: 'Description for Task 1',
-      dueDate: new Date(),
+  it('should retrieve tasks successfully with pagination and filtering', async () => {
+    const tasksData = [
+      {
+        id: '1',
+        topic: 'Task 1',
+        description: 'Description 1',
+        dueDate: new Date(),
+      },
+      {
+        id: '2',
+        topic: 'Task 2',
+        description: 'Description 2',
+        dueDate: new Date(),
+      },
+    ];
+
+    prismaService.task.findMany = jest.fn().mockResolvedValue(tasksData);
+
+    const params = {
+      skip: 0,
+      take: 10,
+      where: { topic: 'Task 1' },
     };
 
-    prismaService.task.findUnique = jest.fn().mockResolvedValue(taskData);
+    const result = await todoService.tasks(params);
 
-    const result = await todoService.getTask(taskId);
-
-    expect(result).toEqual(taskData);
+    expect(result).toEqual(tasksData);
   });
 
-  it('should create a task successfully', async () => {
-    const createTaskDto: CreateTaskDTO = {
-      topic: 'New Task',
-      description: 'Description for New Task',
-      dueDate: new Date(),
-    };
-    const userId = '1';
-
-    prismaService.task.create = jest.fn().mockResolvedValue({
-      id: 'new-task-id',
-      ...createTaskDto,
-    });
-
-    const result = await todoService.createTask(createTaskDto, userId);
-
-    expect(result).toEqual({
-      id: 'new-task-id',
-      ...createTaskDto,
-    });
-  });
-
-  it('should handle errors during task creation', async () => {
-    const createTaskDto: CreateTaskDTO = {
-      topic: 'New Task',
-      description: 'Description for New Task',
-      dueDate: new Date(),
-    };
-    const userId = '1';
-
-    prismaService.task.create = jest
+  it('should handle errors during task retrieval', async () => {
+    prismaService.task.findMany = jest
       .fn()
-      .mockRejectedValue(new Error('Create task error'));
+      .mockRejectedValue(new Error('Task retrieval error'));
+
+    const params = {
+      skip: 0,
+      take: 10,
+    };
 
     try {
-      await todoService.createTask(createTaskDto, userId);
+      await todoService.tasks(params);
     } catch (error) {
       expect(error).toBeInstanceOf(HttpException);
-      expect(error.message).toEqual('Create task error');
+      expect(error.message).toEqual('Failed to retrieve tasks');
       expect(error.getStatus()).toEqual(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   });
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,15 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Prisma, Task } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
-import { UserService } from '../user/user.service';
 import { CreateTaskDTO } from './dtos';
-import { Task } from '@prisma/client';
 
 @Injectable()
 export class TodoService {
-  constructor(
-    private prisma: PrismaService,
-    private userService: UserService,
-  ) {}
+  constructor(private prisma: PrismaService) {}
 
   async getTask(taskId: string): Promise<Task> {
     return await this.prisma.task.findUnique({
@@ -29,6 +25,32 @@ export class TodoService {
       });
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async tasks(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.TaskWhereUniqueInput;
+    where?: Prisma.TaskWhereInput;
+    orderBy?: Prisma.TaskOrderByWithRelationInput;
+  }): Promise<Task[]> {
+    try {
+      const { skip, take, cursor, where, orderBy } = params;
+      const tasks = await this.prisma.task.findMany({
+        skip,
+        take,
+        cursor,
+        where,
+        orderBy,
+      });
+
+      return tasks;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to retrieve tasks',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
