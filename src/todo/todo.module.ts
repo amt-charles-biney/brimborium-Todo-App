f6a90d26935e7ccfec6af6776db3d28e324c5611
Charles Biney <charles.biney@amalitech.org>
2023-09-20 16:13:29 +0000

    feat: get todo controller

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -3,9 +3,10 @@ import { TodoService } from './todo.service';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from 'src/user/user.service';
 import { TodoController } from './todo.controller';
+import { QueryParserService } from 'src/utilities/query-parser.service';
 
 @Module({
-  providers: [TodoService, PrismaService, UserService],
+  providers: [TodoService, PrismaService, UserService, QueryParserService],
   controllers: [TodoController],
 })
 export class TodoModule {}
