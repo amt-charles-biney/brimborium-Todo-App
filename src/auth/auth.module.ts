39da952d0f7a068263fb36074100f98ab7523fc1
Charles Biney <charles.biney@amalitech.org>
2023-09-20 13:22:40 +0000

    feat: authentication controller with tests

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { SessionSerializer } from './local/session.serializer';
 import { LocalStrategy } from './local/local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [UserModule],
   providers: [AuthService, LocalStrategy, SessionSerializer],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,20 +1,63 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserModule } from '../user/user.module';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma.service';
+import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UserModule],
-      providers: [AuthService],
+      providers: [AuthService, UserService, PrismaService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should sign in a user successfully', async () => {
+    const email = 'test@example.com';
+    const password = 'password';
+
+    userService.findUserByEmail = jest.fn().mockResolvedValue({
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
+
+    const result = await authService.signIn(email, password);
+
+    expect(result).toEqual({ email });
+  });
+
+  it('should throw an UnauthorizedException if the email is not found', async () => {
+    const email = 'nonexistent@example.com';
+    const password = 'password';
+
+    userService.findUserByEmail = jest.fn().mockResolvedValue(null);
+
+    try {
+      await authService.signIn(email, password);
+    } catch (error) {
+      expect(error).toBeInstanceOf(UnauthorizedException);
+    }
+  });
+
+  it('should throw an UnauthorizedException if the password is incorrect', async () => {
+    const email = 'test@example.com';
+    const password = 'incorrectPassword';
+
+    userService.findUserByEmail = jest.fn().mockResolvedValue({
+      email,
+      password: await bcrypt.hash('correctPassword', 10),
+    });
+
+    try {
+      await authService.signIn(email, password);
+    } catch (error) {
+      expect(error).toBeInstanceOf(UnauthorizedException);
+    }
   });
 });
