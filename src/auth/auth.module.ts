1a49867bf678292bc0d32d424bde6f69437de82c
Charles Biney <charles.biney@amalitech.org>
2023-09-20 10:01:20 +0000

    feat: initialize session

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { SessionSerializer } from './local/session.serializer';
+import { LocalStrategy } from './local/local.strategy';
 
 @Module({
   imports: [UserModule],
-  providers: [AuthService, SessionSerializer],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
 import * as passport from 'passport';
 import { AppModule } from './app.module';
 
@@ -12,6 +13,19 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: false,
+        httpOnly: process.env.NODE_ENV == 'production' ? true : false,
+        maxAge: 86400000,
+      },
+    }),
+  );
 
   app.use(passport.initialize());
   app.use(passport.session());
