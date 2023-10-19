726173e1bafdd95063a66402fd0681e3e538223b
amt-charles-biney <charles.biney@amalitech.com>
2023-10-19 11:46:47 +0000

    chore: document session serializer class

--- src/auth/local/session.serializer.ts
+++ src/auth/local/session.serializer.ts
@@ -3,12 +3,27 @@ import { Injectable } from '@nestjs/common';
 import { PassportSerializer } from '@nestjs/passport';
 import { User } from '@prisma/client';
 
+/**
+ * Passport session serializer for user sessions.
+ */
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
+  /**
+   * Serialize the user's session data.
+   *
+   * @param {User} user - The user object to be serialized.
+   * @param {(err: Error, user: string) => void} done - The callback function.
+   */
   serializeUser(user: User, done: (err: Error, user: string) => void) {
     done(null, user.id);
   }
 
+  /**
+   * Deserialize the user's session data.
+   *
+   * @param {any} payload - The serialized user data.
+   * @param {(err: Error, payload: string) => void} done - The callback function.
+   */
   deserializeUser(
     payload: any,
     done: (err: Error, payload: string) => void,
