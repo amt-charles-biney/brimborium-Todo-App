a081a52804a3df6d9bfaece8c0e95d59b8c4cc21
amt-charles-biney <charles.biney@amalitech.com>
2023-10-25 15:34:53 +0000

    feat: update todo module

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TodoService } from './todo.service';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from 'src/user/user.service';
-import { TodoController } from './todo.controller';
 import { QueryParserService } from 'src/utilities/query-parser.service';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
+import { TaskStatusModule } from '../jobs/task-status.module';
 
 @Module({
+  imports: [TaskStatusModule],
   providers: [TodoService, PrismaService, UserService, QueryParserService],
   controllers: [TodoController],
 })
