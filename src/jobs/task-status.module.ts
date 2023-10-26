d42af873a3e58910ebaad4b57da77ec0971866e2
amt-charles-biney <charles.biney@amalitech.com>
2023-10-26 09:09:32 +0000

    chore: use .env variables for redis

--- src/jobs/task-status.module.ts
+++ src/jobs/task-status.module.ts
@@ -7,8 +7,8 @@ import { PrismaService } from '../prisma.service';
   imports: [
     BullModule.forRoot({
       redis: {
-        host: 'localhost',
-        port: 6379,
+        host: process.env.REDIS_HOST,
+        port: +process.env.REDIS_PORT,
       },
     }),
     BullModule.registerQueue({
