f35e6222ce8bf24d0abde1f4bddee9d4568a0389
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:16:08 +0000

    feat: update todo controller with task and status update as well as task delete.

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,17 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotificationModule } from '../notification/notification.module';
+import { TaskStatusModule } from '../jobs/task-status.module';
 import { PrismaService } from '../prisma.service';
 import { UserService } from '../user/user.service';
 import { QueryParserService } from '../utilities/query-parser.service';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
-import { TaskStatusModule } from '../jobs/task-status.module';
 
 describe('TodoController', () => {
   let controller: TodoController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TaskStatusModule],
+      imports: [TaskStatusModule, NotificationModule],
       controllers: [TodoController],
       providers: [TodoService, UserService, PrismaService, QueryParserService],
     }).compile();
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,18 +1,21 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  Param,
+  Patch,
   Post,
   Query,
   Request,
   UseGuards,
 } from '@nestjs/common';
-import { Prisma, Task } from '@prisma/client';
+import { Prisma, type Status, type Task } from '@prisma/client';
 import { Request as ExpressRequest } from 'express';
 import { AuthenticatedGuard } from '../auth/auth.guard';
-import type { CreateTaskDTO } from './dtos';
-import { TodoService } from './todo.service';
 import { QueryParserService } from '../utilities/query-parser.service';
+import type { CreateTaskDTO, UpdateTaskDTO } from './dtos';
+import { TodoService } from './todo.service';
 
 /**
  * Controller for managing to-do task operations.
@@ -72,5 +75,55 @@ export class TodoController {
       ) as Prisma.TaskOrderByWithRelationInput,
       where: this.queryParser.parseQuery(where) as Prisma.TaskWhereInput,
     });
+  }
+
+  /**
+   * Update a to-do task by ID.
+   *
+   * @param id - The ID of the task to update.
+   * @param status - The new status to set for the task.
+   * @returns {Promise<Task>} A promise that resolves to the updated task.
+   */
+  @UseGuards(AuthenticatedGuard)
+  @Patch('status/:id')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() data: { status: Status },
+  ): Promise<Task> {
+    return this.todoService.updateTaskStatus(id, data.status);
+  }
+
+  /**
+   * Update the status of a to-do task by ID.
+   *
+   * @param id - The ID of the task to update.
+   * @param status - The new status to set for the task.
+   * @returns {Promise<Task>} A promise that resolves to the updated task.
+   */
+  @UseGuards(AuthenticatedGuard)
+  @Patch(':id')
+  updateTask(
+    @Param('id') id: string,
+    @Body() data: UpdateTaskDTO,
+  ): Promise<Task> {
+    return this.todoService.updateTask(id, data);
+  }
+
+  /**
+   * Delete a to-do task by ID.
+   *
+   * @param id - The ID of the task to delete.
+   * @returns {Promise<void>} A promise that resolves once the task is deleted.
+   */
+  @UseGuards(AuthenticatedGuard)
+  @Delete(':id')
+  async deleteTask(
+    @Param('id') id: string,
+  ): Promise<{ status: boolean; message: string }> {
+    await this.todoService.deleteTask(id);
+    return {
+      status: true,
+      message: 'User deleted successfully',
+    };
   }
 }
